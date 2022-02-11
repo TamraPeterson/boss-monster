15f21ec1788e5bab94c8a042f1cf46dd5746df19
Katie McCauley <katiemccau02@gmail.com>
2022-02-11 00:43:51 -0700

    commit

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
-const harry = {
-  health: 100,
-  images: {
-    healthy: 'https://cdn.vox-cdn.com/thumbor/QK6pVSgUPodR2kEFpvl0BL15GrI=/0x0:1920x1080/1200x800/filters:focal(799x210:1105x516)/cdn.vox-cdn.com/uploads/chorus_image/image/70311477/harrypotter.0.jpg',
-    hurt: 'https://media.vanityfair.com/photos/54ca8fa97e4b004120bb20b4/16:9/w_1280,c_limit/image.jpg',
-    dead: 'https://i.chzbgr.com/full/8974606336/h3D5083CB/crazy-fan-theory-on-whether-harry-potter-actually-died-or-not'
-  }
-}
 
-const tom = {
-  health: 100,
-  images: {
-    healthy: 'https://www.looper.com/img/gallery/dumbledores-history-with-voldemort-explained/intro-1641410297.jpg',
-    hurt: 'https://imageio.forbes.com/specials-images/imageserve/77202458/The-Real-Look-of-Lord-Voldemort-from--Harry-Potter-and-the-Goblet-of-Fire-/960x0.jpg?fit=bounds&format=jpg&width=960',
-    dead: 'https://static0.srcdn.com/wordpress/wp-content/uploads/2019/11/Voldemorts-Death.jpg?q=50&fit=crop&w=450&h=225&dpr=1.5',
-  }
-}
 
 const characters = {
-  tom,
-  harry,
+  harry: {
+    health: 100,
+    images: 'https://cdn.vox-cdn.com/thumbor/QK6pVSgUPodR2kEFpvl0BL15GrI=/0x0:1920x1080/1200x800/filters:focal(799x210:1105x516)/cdn.vox-cdn.com/uploads/chorus_image/image/70311477/harrypotter.0.jpg'
+  },
+  tom: {
+    health: 100,
+    images: 'https://www.looper.com/img/gallery/dumbledores-history-with-voldemort-explained/intro-1641410297.jpg'
+  }
 }
 
 function drawHarry() {
@@ -33,103 +23,92 @@ function drawHarry() {
   }
   template += `
    
-        <h3 class="p-3">Harry⚡</h3>
-        <img class="img img-fluid rounded clickable" 
-          src="${image}"
-          alt="">
-        <h5 class="p-3">Health: <span id="health">${harry.health}</span></h5>
-        <div class="progress">
-          <div class="progress-bar progress-bar-striped bg-danger" role="progressbar" style="width: ${harry.health}%"
+    <h3 class="p-3">Harry⚡</h3>
+      <img class="img img-fluid rounded clickable" src="${image}" alt="">
+      <h5 class="p-3">Health: <span id="health">${harry.health}</span></h5>
+      <div class="progress">
+        <div class="progress-bar progress-bar-striped bg-danger" role="progressbar" style="width: ${harry.health}%"
             aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
-          </div>
         </div>
+      </div>
     
   `
   document.getElementById('harry').innerHTML = template
 }
 
 
-function drawChar(name) {
+function drawChar() {
   let template = ""
-  let img = ""
-  let char = characters[name]
-  if (char.health > 50) {
-    img = char.images.healthy
-  } else if (char.health > 0) {
-    img = char.images.healthy
-  } else {
-    img = char.image.healthy
-  }
-  template += 
-}
-
-function drawTom() {
-  let template = ''
-  let image = ''
-  if (tom.health > 50) {
-    image = tom.images.healthy
-  } else if (tom.health > 0) {
-    image = tom.images.hurt
-  } else {
-    image = tom.images.dead
-  }
-  template += `
-  
-    <h3 class="p-3">Voldemort🐍</h3>
-    <img class="img img-fluid rounded clickable" onclick="atk()"
-      src="${image}"
+  for (let key in characters) {
+    let character = characters[key]
+    console.log(key)
+    template = `
+    
+    <h3 class="p-3">${key}</h3>
+      <img class="img img-fluid rounded clickable" onclick="attack(${key == "tom" ? "tom" : ""})" src="${character.images}"
       alt="">
-    <h5 class="p-3">Health: <span id="health">${tom.health}</span></h5>
-    <div class="progress">
-      <div class="progress-bar progress-bar-striped bg-danger" role="progressbar" style="width: ${tom.health}%"
-        aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+      <h5 class="p-3">Health: <span id="health">${character.health}</span></h5>
+      <div class="progress">
+        <div class="progress-bar progress-bar-striped bg-danger" role="progressbar" style="width: ${character.health}%"
+          aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
+        </div>
       </div>
-    </div>
 
-  `
-  document.getElementById('tom').innerHTML = template
+    `
+
+    document.getElementById(key).innerHTML = template
+  }
+}
+
+function updateChart(name) {
+  let bossElem = document.getElementById(name)
+  let bar = bossElem.querySelector('.progress-bar')
+  bar.style.width = characters[name].health + '%'
 
 }
 
-function atk() {
-  if (harry.health > 0) {
-    tom.health -= Math.floor(Math.random() * 10)
-    if (tom.health <= 0) {
-      tom.health = 0
+// this function is invoked when you click on voldermort, which makes health decrease and if it is 0 then it equals 0, but harry potter has to have health above 100
+function attack(boss) {
+  console.log(boss.health)
+  if (boss.health > 0) {
+    boss.health -= Math.floor(Math.random() * 10)
+    console.log(boss.health);
+    if (boss.health <= 0) {
+      boss.health = 0
     }
   }
-  console.log(tom.health)
-  drawTom()
+  drawChar()
 }
 
-function dmg() {
-  harry.health -= Math.floor(Math.random() * 10)
-  if (harry.health <= 0) {
-    harry.health = 0
+// this fucntion is set on a interval below everything, but it decreases harrys health on a set interval
+function damage() {
+  characters.harry.health -= Math.floor(Math.random() * 10)
+  console.log(characters.harry.health);
+  if (characters.harry.health <= 0) {
+    characters.harry.health = 0
+
   }
-  drawHarry()
+  drawChar()
 }
 
+
+
+// stops the interval of harry health decreasing when valmor is dead
 function stopDmg() {
   if (tom.health == 0) {
     clearInterval(dmgInterval)
   }
 }
 
-function killCount() {
-  if (tom.health == 0) {
-
-  }
-}
-
+// the reset button when the game is over
 function reset() {
-  tom.health = 100
-  harry.health = 100
-  drawHarry()
-  drawTom()
+  characters.tom.health = 100
+  characters.harry.health = 100
+  drawChar()
 }
 
-drawHarry()
-drawTom()
-let dmgInterval = setInterval(dmg, 500)
+
+drawChar()
+let dmgInterval = setInterval(damage, 500)
 let stopInterval = setInterval(stopDmg, 500)
+
